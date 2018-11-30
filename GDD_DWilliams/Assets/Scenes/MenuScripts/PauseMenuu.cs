9d2256d18e9fe8c7e3d5339c1acbed5c9c62cb37
Danielle Williams <dwilliams72csub.edu>
2018-11-29 16:53:15 -0800

    created resume page

---
+++ GDD_DWilliams/Assets/Scenes/MenuScripts/PauseMenuu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuu : MonoBehaviour {
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuuUI; 
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+	 }
+}
+
+    public void Resume()
+    {
+        pauseMenuuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+
+        void Pause ()
+        {
+            pauseMenuuUI.SetActive(true);
+            Time.timeScale = 0f;
+            GameIsPaused = true;
+        }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("001_MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+
+    }
+
+
